90aa2bcd7ce2e82a3effbc5a745a755f4cd2ec2a
rebaz36 <rebaz415@gmail.com>
2022-10-26 20:40:14 +0300

    refactor to functional component & use hooks

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,63 +1,68 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+export default function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick = (e) => {
-    const result = calculate(this.state, e.target.textContent);
-    this.setState(result);
+  const handleClick = (e) => {
+    // const result = calculate(this.state, e.target.textContent);
+    // this.setState(result);
+    setState((state) => calculate(state, e.target.textContent));
   };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="calculator">
-        <div className="calculator-display">{ next || total }</div>
-        <div className="calculator-keypad">
-          <div className="input-keys">
-            <div className="function-keys">
-              <button onClick={this.handleClick} type="button" className="calculator-key key-clear">AC</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-sign">±</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-percent">%</button>
-            </div>
-            <div className="digit-keys">
-              <button onClick={this.handleClick} type="button" className="calculator-key key-7">7</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-8">8</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-9">9</button>
+  return (
+    <div className="calculator">
 
-              <button onClick={this.handleClick} type="button" className="calculator-key key-4">4</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-5">5</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-6">6</button>
+      <div className="calculator-display">{ state.next || state.total }</div>
+      <div className="calculator-keypad">
 
-              <button onClick={this.handleClick} type="button" className="calculator-key key-1">1</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-2">2</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-3">3</button>
+        <div className="input-keys">
 
-              <button onClick={this.handleClick} type="button" className="calculator-key key-0">0</button>
-              <button onClick={this.handleClick} type="button" className="calculator-key key-dot">.</button>
+          <div className="function-keys">
 
-            </div>
+            <button onClick={handleClick} type="button" className="calculator-key key-clear">AC</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-sign">±</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-percent">%</button>
+
           </div>
-          <div className="operator-keys">
-            <button onClick={this.handleClick} type="button" className="calculator-key key-divide orange-key">÷</button>
-            <button onClick={this.handleClick} type="button" className="calculator-key key-multiply orange-key">x</button>
-            <button onClick={this.handleClick} type="button" className="calculator-key key-subtract orange-key">-</button>
-            <button onClick={this.handleClick} type="button" className="calculator-key key-add orange-key">+</button>
-            <button onClick={this.handleClick} type="button" className="calculator-key key-equals orange-key">=</button>
+
+          <div className="digit-keys">
+
+            <button onClick={handleClick} type="button" className="calculator-key key-7">7</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-8">8</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-9">9</button>
+
+            <button onClick={handleClick} type="button" className="calculator-key key-4">4</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-5">5</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-6">6</button>
+
+            <button onClick={handleClick} type="button" className="calculator-key key-1">1</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-2">2</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-3">3</button>
+
+            <button onClick={handleClick} type="button" className="calculator-key key-0">0</button>
+            <button onClick={handleClick} type="button" className="calculator-key key-dot">.</button>
+
           </div>
+
         </div>
 
+        <div className="operator-keys">
+
+          <button onClick={handleClick} type="button" className="calculator-key key-divide orange-key">÷</button>
+          <button onClick={handleClick} type="button" className="calculator-key key-multiply orange-key">x</button>
+          <button onClick={handleClick} type="button" className="calculator-key key-subtract orange-key">-</button>
+          <button onClick={handleClick} type="button" className="calculator-key key-add orange-key">+</button>
+          <button onClick={handleClick} type="button" className="calculator-key key-equals orange-key">=</button>
+
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
