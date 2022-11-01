19fc688a28ac98b06f6f03b956eb30985be687de
rebaz36 <rebaz415@gmail.com>
2022-11-01 23:07:18 +0300

    Optmize Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Calculator from './component/Calculator';
-import Layout from './component/Layout/Layout';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import Calculator from './pages/Calculator/Calculator';
+import Home from './pages/Home/Home';
+import Qoute from './pages/Qoute/Qoute';
+// import Layout from './component/Layout/Layout';
+import NavBar from './component/Layout/NavBar';
+import Footer from './component/Layout/Footer';
 
 export default function App() {
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/" component={Calculator} />
-    //   </Switch>
-    // </Router>
-    <Layout />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Qoute />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
