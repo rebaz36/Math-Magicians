ce6c86d841d6a9cedef2d425ed37f20b095d8d68
rebaz36 <rebaz415@gmail.com>
2022-11-01 23:04:24 +0300

    Add Footer Content

--- src/component/Layout/Footer.js
+++ src/component/Layout/Footer.js
@@ -3,6 +3,17 @@ import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      Made with ❤️ by
+      {' '}
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/r36/"
+        rel="noreferrer"
+        className="footer__link"
+      >
+        &nbsp;Rebaz Farid Noori
+      </a>
+    </div>
   );
 }
