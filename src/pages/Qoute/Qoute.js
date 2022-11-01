94afc5c6bf3ab95b444cfe66a0f9485d0ffd90c8
rebaz36 <rebaz415@gmail.com>
2022-11-01 23:07:09 +0300

    Add Qoutes Page

--- src/pages/Qoute/Qoute.js
+++ src/pages/Qoute/Qoute.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GetQoutes from './QouteData';
 import './Qpute.css';
 
 export default function Qoute() {
+  const qoutes = new GetQoutes();
+  const [qoute, setQoute] = useState(qoutes.getRandomQoute());
+
+  const handleQoute = () => {
+    setQoute(qoutes.getRandomQoute());
+  };
   return (
-    <div>Qoute</div>
+    <div className="Qoute-Container">
+      <div className="Qoute-content">
+        <h1 className="Qoute-Title">Qoute of the Day</h1>
+        <h2 className="Qoute-Des">{qoute.quote}</h2>
+        <div className="info-container">
+          <p className="Qoute-Author">{qoute.author}</p>
+          <button className="Qoute-btn" type="button" onClick={handleQoute}>
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
